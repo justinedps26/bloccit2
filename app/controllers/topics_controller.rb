9af8ce0e423a625823313235e04212f7da24d62f
Justine Dupuis <justine.dps26@gmail.com>
2017-03-09 18:35:19 +0100

    public & private topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,11 +4,16 @@ class TopicsController < ApplicationController
   before_action :authorize_user, except: [:index, :show]
 
   def index
-      @topics = Topic.all
+      @topics = Topic.visible_to(current_user)
   end
 
   def show
     @topic = Topic.find(params[:id])
+
+    unless @topic.public || current_user
+      flash[:alert] = "You must be signed in to view private topics"
+      redirect_to new_session_path
+    end
   end
 
   def new
@@ -65,7 +70,7 @@ class TopicsController < ApplicationController
    end
 
    def authorize_user
-     unless current_user.admin? 
+     unless current_user.member?
        flash[:alert] = "You must be an admin to do that"
        redirect_to topics_path
      end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,24 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
+
+  # Scope is a method that generates methods
+  # Its first argument is the name of the method to generate
+  # Its second argument provides the logic to run:
+        # We use the lambda syntax (-> with a block { ... }) to implement
+        # the method's logic
+  # the visible_to scope will run the code inside the lambda on the relation
+  # or class on which it's called
+  # To make it work properly we require an if statement in TopicsController to
+  # determine if a user is signed in.
+  # lambda block's syntax:
+      # This syntax is called the ternary operator:
+      # if_this_is_a_true_value ? then_the_result_is_this : else_it_is_this
+      # It's like an if statement like:
+          # if A then B else C end is the same as a ? b : c
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  # It now returns the equivalent of Topic.all or Topic.where(public: true)
+  # depending on the value of user (present or nil)
+
+
 end
