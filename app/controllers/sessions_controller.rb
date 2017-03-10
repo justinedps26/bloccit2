637b1363e286bb62da0b6edfcf827f7704d3e71f
Justine Dupuis <justine.dps26@gmail.com>
2017-03-10 12:40:30 +0100

    styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       create_session(user)
-      flash[:notice] = "Welcome, #{user.name}!"
       redirect_to topics_path
     else
       flash.now[:alert] = "Invalid email/password combination"
@@ -18,7 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     destroy_session(current_user)
-    flash[:notice] = "You've been signed out, come back soonn!"
     redirect_to root_path
   end
 end
