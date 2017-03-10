54fc93a7c3f22692165332ad7021675d8536ebb3
Justine Dupuis <justine.dps26@gmail.com>
2017-03-10 21:35:19 +0100

    markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,14 +11,14 @@ module ApplicationHelper
       filter_html:     true,
       hard_wrap:       true,
       link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true,
-      fenced_code_blocks: true
+      space_after_headers: true
     }
 
     extensions = {
       autolink:           true,
       superscript:        true,
-      disable_indented_code_blocks: false
+      disable_indented_code_blocks: false,
+      fenced_code_blocks: true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
