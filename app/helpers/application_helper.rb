c286932fc75782d4eb1f136b40c1195c7ef8d908
Justine Dupuis <justine.dps26@gmail.com>
2017-03-10 22:00:57 +0100

    styling edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,15 +14,17 @@ module ApplicationHelper
       space_after_headers: true
     }
 
-    extensions = {
-      autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: false,
-      fenced_code_blocks: true
-    }
-
     renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       no_intra_emphasis: true,
+                                       fenced_code_blocks: true,
+                                       disable_indented_code_blocks: true,
+                                       autolink: true,
+                                       tables: true,
+                                       underline: true,
+                                       highlight: true
+                                      )
+    return markdown.render(text).html_safe
 
     markdown.render(text).html_safe
   end
